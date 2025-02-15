9477dc7694c24b854c7ea858682609d1e4c492ef
Aabhi-k <aabhigyankumar123@gmail.com>
2025-02-15 19:34:05 +0530

    finished kanban

---
+++ client/src/utils/axiosInstance.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import store from "./store";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8080/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = store.getState().auth.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      delete config.headers.Authorization;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
